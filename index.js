46763378ead7489bfb8ae020187b4bee59402799
surya <surya.phani1729@gmail.com>
2019-08-20 15:25:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
 const express = require("express");
 const mongoose = require('mongoose')
 let mongodb = require('./database');
+var bodyParser = require('body-parser')
+
 
 const app = express();
 
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.json({ extended: false }));
+
 const port = process.env.PORT || 8080;
+var MyModel = mongoose.model('oqsapp', new mongoose.Schema({ title : String,quiz:Array,answers:Array }));
 
 //get all users
-app.get("/",  async(req, res) => {
+app.get("/",   (req, res) => {
 
-var MyModel = mongoose.model('oqsapp', new Schema({ title : String,quiz:Array,answers:Array }));
+//var MyModel = mongoose.model('oqsapp', new mongoose.Schema({ title : String,quiz:Array,answers:Array }));
 
 // Works
-await MyModel.findOne(function(error, result) { res.json(result); });
+MyModel.find(function(error, result) { res.json(result); });
 
 
 
 
 });
+
+app.post("/add",(req,res) => {
+
+
+
+     var title = req.params.title;
+var data = new MyModel({ title: title, quiz:[],answers:[] });
+
+data.save(function (err) {
+
+ MyModel.find(function(error, result) { res.json(result); });
+
+});
+
+
+})
 
 app.listen(port);
